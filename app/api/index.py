c6089cae0d39c3b51b53a51e5475b903ad576c63
vishnu1542 <vishnuvardhankasturi2@gmail.com>
2026-09-07 00:12:25 +0530

    updated api/index.py

--- app/api/index.py
+++ app/api/index.py
@@ -1,18 +1,1 @@
-from fastapi import FastAPI
 from app.main import app
-
-app = FastAPI()
-
-
-@app.get("/api")
-def root():
-    return {
-        "message": "GitHub Repo RAG Assistant API is running"
-    }
-
-
-@app.get("/api/health")
-def health():
-    return {
-        "status": "ok"
-    }
